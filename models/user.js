e97316e4d62ee1a9ad97445edf8f9f0fe11c08c1
Jorge Ore <jore24@autonoma.edu.pe>
2022-10-18 18:24:33 -0500

    up folders

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema(
         },
         role: {
             type: mongoose.Types.ObjectId
+        },
+        active: {
+            type: Boolean,
+            default: false
         }
     },
     {
